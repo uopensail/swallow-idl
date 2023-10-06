af69eb0696270860de26dd744cba13e9fe497998
kungjim <7629149+kungjim@users.noreply.github.com>
2023-10-06 19:33:51 +0800

    add proto

---
+++ proto/swallowapi/genproto.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# brew upgrade protobuf
+# brew link --overwrite protobuf
+# go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+# go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+
+protoc -I=. -I=$GOPATH/src --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    *.proto
